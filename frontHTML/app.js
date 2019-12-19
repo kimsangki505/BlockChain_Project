69960bd5f22f65d8f62880e7838fc851355aceec
kyu <21500629@handong.edu>
2019-12-19 19:17:36 +0900

    Front Update

--- frontHTML/app.js
+++ frontHTML/app.js
@@ -19,14 +19,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 var owner_address = "0xDC8578AE2b48818bdFC24f3ff80b404ECbC486Dc";
 var action_address = "0x670B03960cb54737044dCEFCbF6D6A1696DAc437";
 
-var action_Contract_abi = [{"constant":false,"inputs":[{"internalType":"string","name":"userID_A","type":"string"},{"internalType":"string","name":"cityName_A","type":"string"},{"internalType":"uint32","name":"a_solider","type":"uint32"},{"internalType":"string","name":"userID_B","type":"string"},{"internalType":"string","name":"cityName_B","type":"string"}],"name":"attack","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"string","name":"userID","type":"string"}],"name":"colletTax","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"string","name":"userID","type":"string"},{"internalType":"string","name":"cityName","type":"string"}],"name":"heal","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"ethereumAddress","type":"address"},{"internalType":"string","name":"userID","type":"string"},{"internalType":"string","name":"userPassword","type":"string"}],"name":"makeAccount","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"string","name":"userID","type":"string"},{"internalType":"string","name":"cityName","type":"string"}],"name":"makeFortress","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"string","name":"userID","type":"string"},{"internalType":"string","name":"cityName","type":"string"},{"internalType":"uint32","name":"number","type":"uint32"}],"name":"produceSolider","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"string","name":"userID","type":"string"},{"internalType":"string","name":"userPassword","type":"string"}],"name":"login","outputs":[],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"mapArray","outputs":[{"internalType":"string","name":"owner","type":"string"},{"internalType":"bool","name":"capital","type":"bool"},{"internalType":"uint32","name":"hp","type":"uint32"},{"internalType":"string","name":"name","type":"string"},{"internalType":"uint256","name":"position","type":"uint256"},{"internalType":"bool","name":"fortress","type":"bool"},{"internalType":"uint32","name":"numberOfsoldiers","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"returnMapData","outputs":[{"components":[{"internalType":"string","name":"owner","type":"string"},{"internalType":"bool","name":"capital","type":"bool"},{"internalType":"uint32","name":"hp","type":"uint32"},{"internalType":"string","name":"name","type":"string"},{"internalType":"uint256","name":"position","type":"uint256"},{"internalType":"bool","name":"fortress","type":"bool"},{"internalType":"uint32","name":"numberOfsoldiers","type":"uint32"}],"internalType":"structmapInfo.city[]","name":"map","type":"tuple[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"string","name":"userID","type":"string"}],"name":"returnUserData","outputs":[{"components":[{"internalType":"address","name":"ethereumAccount","type":"address"},{"internalType":"string","name":"userID","type":"string"},{"internalType":"string","name":"userPassword","type":"string"},{"internalType":"bool","name":"survival","type":"bool"},{"internalType":"uint32","name":"money","type":"uint32"},{"internalType":"uint256","name":"taxTime","type":"uint256"}],"internalType":"structuserStatus.userData","name":"user","type":"tuple"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"users","outputs":[{"internalType":"address","name":"ethereumAccount","type":"address"},{"internalType":"string","name":"userID","type":"string"},{"internalType":"string","name":"userPassword","type":"string"},{"internalType":"bool","name":"survival","type":"bool"},{"internalType":"uint32","name":"money","type":"uint32"},{"internalType":"uint256","name":"taxTime","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
+var action_Contract_abi = [{"constant":true,"inputs":[{"internalType":"string","name":"userID","type":"string"}],"name":"returnUserData","outputs":[{"components":[{"internalType":"address","name":"ethereumAccount","type":"address"},{"internalType":"string","name":"userID","type":"string"},{"internalType":"string","name":"userPassword","type":"string"},{"internalType":"bool","name":"survival","type":"bool"},{"internalType":"uint32","name":"money","type":"uint32"},{"internalType":"uint256","name":"taxTime","type":"uint256"}],"internalType":"structuserStatus.userData","name":"user","type":"tuple"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"string","name":"userID","type":"string"}],"name":"colletTax","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"string","name":"userID","type":"string"},{"internalType":"string","name":"cityName","type":"string"}],"name":"makeFortress","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"mapArray","outputs":[{"internalType":"string","name":"owner","type":"string"},{"internalType":"bool","name":"capital","type":"bool"},{"internalType":"uint32","name":"hp","type":"uint32"},{"internalType":"string","name":"name","type":"string"},{"internalType":"uint256","name":"position","type":"uint256"},{"internalType":"bool","name":"fortress","type":"bool"},{"internalType":"uint32","name":"numberOfsoldiers","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"users","outputs":[{"internalType":"address","name":"ethereumAccount","type":"address"},{"internalType":"string","name":"userID","type":"string"},{"internalType":"string","name":"userPassword","type":"string"},{"internalType":"bool","name":"survival","type":"bool"},{"internalType":"uint32","name":"money","type":"uint32"},{"internalType":"uint256","name":"taxTime","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"returnMapData","outputs":[{"components":[{"internalType":"string","name":"owner","type":"string"},{"internalType":"bool","name":"capital","type":"bool"},{"internalType":"uint32","name":"hp","type":"uint32"},{"internalType":"string","name":"name","type":"string"},{"internalType":"uint256","name":"position","type":"uint256"},{"internalType":"bool","name":"fortress","type":"bool"},{"internalType":"uint32","name":"numberOfsoldiers","type":"uint32"}],"internalType":"structmapInfo.city[]","name":"map","type":"tuple[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"string","name":"userID","type":"string"},{"internalType":"string","name":"userPassword","type":"string"}],"name":"login","outputs":[],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"string","name":"userID_A","type":"string"},{"internalType":"string","name":"cityName_A","type":"string"},{"internalType":"uint32","name":"a_solider","type":"uint32"},{"internalType":"string","name":"userID_B","type":"string"},{"internalType":"string","name":"cityName_B","type":"string"}],"name":"attack","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"ethereumAddress","type":"address"},{"internalType":"string","name":"userID","type":"string"},{"internalType":"string","name":"userPassword","type":"string"}],"name":"makeAccount","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"string","name":"userID","type":"string"},{"internalType":"string","name":"cityName","type":"string"},{"internalType":"uint32","name":"number","type":"uint32"}],"name":"produceSolider","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"string","name":"userID","type":"string"},{"internalType":"string","name":"cityName","type":"string"}],"name":"heal","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}];
 
 var action_Contract = new web3.eth.Contract(action_Contract_abi, action_address, { from: owner_address });
 
 var nationFlag =  ['kor','us','uk','cn','ru','bel','fra','ger','net','swe'];
 var userList = [];
+
+
 app.get('/', function (req, res) {
-    res.writeHead(301, { Location: 'http://127.0.0.1:3000/login' });
+    res.writeHead(301, { Location: 'http://172.17.222.130:3000/login' });
     res.end();
 });
 
@@ -39,17 +41,16 @@ app.post('/login_post', function (req, res) {
     userList.push(Username);
     var Username = req.body.Username;
 	var Password = req.body.Password;
-	
-	
+    	
 
     action_Contract.methods.login(Username, Password).call({ from: owner_address })
         .then(function (value) {
-            res.writeHead(301, { Location: 'http://127.0.0.1:3000/main/' +  Username });
+            res.writeHead(301, { Location: 'http://172.17.222.130:3000/main/' +  Username });
             res.write(Username);
             res.end();
         }).catch(function (error) {
             console.log(error);
-            res.writeHead(301, { Location: 'http://127.0.0.1:3000/login' });
+            res.writeHead(301, { Location: 'http://172.17.222.130:3000/login' });
             res.end();
         });
 });
@@ -65,21 +66,23 @@ app.post('/signup_post', function (req, res) {
     var Password = req.body.Password;
     var Repeat_Password = req.body.Repeat_Password
 
+    console.log("---------------> "+Username);
+
     if (Repeat_Password == Repeat_Password) {
         action_Contract.methods.makeAccount(account, Username, Password).send({ from: account, gas:500000 })
             .then(function (value) {
                 console.log(value);
-                res.writeHead(301, { Location: 'http://127.0.0.1:3000/login' });
+                res.writeHead(301, { Location: 'http://172.17.222.130:3000/login' });
                 res.end();
             }).catch(function (error) {
                 console.log(error);
-                res.writeHead(301, { Location: 'http://127.0.0.1:3000/signup' });
+                res.writeHead(301, { Location: 'http://172.17.222.130:3000/signup' });
                 res.end();
             });
     }
     else
     {
-        res.writeHead(301, { Location: 'http://127.0.0.1:3000/signup' });
+        res.writeHead(301, { Location: 'http://172.17.222.130:3000/signup' });
         res.end();
     }
 });
@@ -139,7 +142,7 @@ app.get('/main/:userID/', function (req, res) {
     })
     .catch(function(error){
         console.log(error);
-        res.writeHead(301, { Location: 'http://127.0.0.1:3000/main/' + userid});
+        res.writeHead(301, { Location: 'http://172.17.222.130:3000/main/' + userid});
         res.end();
     });
 });
@@ -157,11 +160,11 @@ app.post('/produceSolider/:userID/', function (req, res) {
     action_Contract.methods.produceSolider(userid, usercity.trim(), numOfsolider).send({ from: useraccount, gas:500000 })
     .then(function(value){
         console.log(value);
-        res.writeHead(301, { Location: 'http://127.0.0.1:3000/main/' + userid });
+        res.writeHead(301, { Location: 'http://172.17.222.130:3000/main/' + userid });
         res.end();
     }).catch(function(error){
         console.log(error);
-        res.writeHead(301, { Location: 'http://127.0.0.1:3000/main/' + userid});
+        res.writeHead(301, { Location: 'http://172.17.222.130:3000/main/' + userid});
         res.end();
     });
 });
@@ -174,10 +177,10 @@ app.post('/makeFortress/:userID/', function (req, res) {
 
     action_Contract.methods.makeFortress(userid, usercity.trim()).send({ from: useraccount, gas:500000 })
     .then(function(value){
-        res.writeHead(301, { Location: 'http://127.0.0.1:3000/main/' + userid});
+        res.writeHead(301, { Location: 'http://172.17.222.130:3000/main/' + userid});
         res.end();
     }).catch(function(error){
-        res.writeHead(301, { Location: 'http://127.0.0.1:3000/main/' + userid});
+        res.writeHead(301, { Location: 'http://172.17.222.130:3000/main/' + userid});
         res.end();
     });
 });
@@ -188,10 +191,10 @@ app.post('/colletTax/:userID/', function (req, res) {
     
     action_Contract.methods.colletTax(userid).send({ from: useraccount, gas:500000 })
     .then(function(value){
-        res.writeHead(301, { Location: 'http://127.0.0.1:3000/main/' + userid});
+        res.writeHead(301, { Location: 'http://172.17.222.130:3000/main/' + userid});
         res.end();
     }).catch(function(error){
-        res.writeHead(301, { Location: 'http://127.0.0.1:3000/main/' + userid});
+        res.writeHead(301, { Location: 'http://172.17.222.130:3000/main/' + userid});
         res.end();
     });
 });
@@ -208,10 +211,10 @@ app.post('/attack/:userID/', function (req, res) {
     // action_Contract.methods.attack(userid_a, usercity_a.trim(), numOfsolider, user_city_b[0].trim(), user_city_b[1].trim()).send({ from: useraccount, gas:500000 })
     action_Contract.methods.attack(userid_a, usercity_a.trim(), numOfsolider, user_city_b[0].trim(), user_city_b[3].trim()).send({ from: useraccount, gas:500000 })
     .then(function(value){
-        res.writeHead(301, { Location: 'http://127.0.0.1:3000/main/' + userid_a});
+        res.writeHead(301, { Location: 'http://172.17.222.130:3000/main/' + userid_a});
         res.end();
     }).catch(function(error){
-        res.writeHead(301, { Location: 'http://127.0.0.1:3000/main/' + userid_a});
+        res.writeHead(301, { Location: 'http://172.17.222.130:3000/main/' + userid_a});
         res.end();
     });
 });
@@ -224,10 +227,10 @@ app.post('/heal/:userID/', function (req, res) {
 
     action_Contract.methods.heal(userid, usercity.trim()).send({ from: useraccount, gas:500000 })
     .then(function(value){
-        res.writeHead(301, { Location: 'http://127.0.0.1:3000/main/' + userid});
+        res.writeHead(301, { Location: 'http://172.17.222.130:3000/main/' + userid});
         res.end();
     }).catch(function(error){
-        res.writeHead(301, { Location: 'http://127.0.0.1:3000/main/' + userid});
+        res.writeHead(301, { Location: 'http://172.17.222.130:3000/main/' + userid});
         res.end();
     });
 });
